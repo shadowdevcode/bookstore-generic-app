de7ac59ccf7685455364aba3e5ed22b461410b8b
Shadowdevcode <vijay.b.sehgal@gmail.com>
2019-01-24 22:46:13 +0530

    changed settings.py file

--- mybooksite/settings.py
+++ mybooksite/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
