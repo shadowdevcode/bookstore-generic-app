528c32b1aed076b6063cf3f03f3f3a5fc5f97ca1
Shadowdevcode <vijay.b.sehgal@gmail.com>
2019-01-24 23:36:27 +0530

    added app name in Staticfiles_dir in settings.py file

--- mybooksite/settings.py
+++ mybooksite/settings.py
@@ -147,7 +147,7 @@ STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'books/static'),
 )
 
 #  Add configuration for static files storage using whitenoise
